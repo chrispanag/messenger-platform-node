03d492ccbd5eaa75e4da388d5a68a8d1b80f467d
Nikos <nikpolik38@gmail.com>
2018-04-03 16:13:17 +0300

    Stuff

--- src/broadcast.js
+++ src/broadcast.js
@@ -14,12 +14,12 @@ function validateJson(json) {
 function estimateBroadcast(qs, labelId) {
     const options = Object.assign({}, baseOptions);
     if(label) options.custom_label_id = labelId;
-    return fetch(`https://graph.facebook.com/v2.11/me/broadcast_reach_estimations?access_token=${qs}}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/me/broadcast_reach_estimations?${qs}}`, options)
     .then(rsp => rsp.json())
     .then(json => {
         validateJson(json);
         const { reach_estimation_id } = json;
-        return fetch(`https://graph.facebook.com/v2.11/${reach_estimation_id}?access_token=${qs}`, baseOptions)
+        return fetch(`https://graph.facebook.com/v2.11/${reach_estimation_id}?${qs}`, baseOptions)
     })
     .then(rsp => rsp.json())
     .then(json => validateJson(json))
@@ -29,7 +29,7 @@ function createLabel(qs, name) {
     const options = Object.assign({}, baseOptions, {
         name
     });
-    return fetch(`https://graph.facebook.com/v2.11/me/custom_labels?access_token=${qs}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/me/custom_labels?${qs}`, options)
     .then(rsp => rsp.json())
     .then(json => validateJson(json));
 }
@@ -38,7 +38,7 @@ function associateToLabel(qs, PSID, labelId) {
     const options = Object.assign({}, baseOptions, {
         user: PSID
     });
-    return fetch(`https://graph.facebook.com/v2.11/${labelId}/label?access_token=${qs}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/${labelId}/label?${qs}`, options)
     .then(rsp => rsp.json())
     .then(json => validateJson(json));
 }
@@ -48,13 +48,13 @@ function removeLabel(qs, PSID, labelId) {
         user: PSID,
         method: 'DELETE'
     });
-    return fetch(`https://graph.facebook.com/v2.11/${labelId}/label?access_token=${qs}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/${labelId}/label?${qs}`, options)
     .then(rsp => rsp.json())
     .then(json => validateJson(json)); 
 }
 
 function getUserLabels(qs, id) {
-    return fetch(`https://graph.facebook.com/v2.11/${id}/custom_labels?access_token=${qs}`, {
+    return fetch(`https://graph.facebook.com/v2.11/${id}/custom_labels?${qs}`, {
         method: 'GET'
     })
     .then(rsp => rsp.json())
@@ -62,7 +62,7 @@ function getUserLabels(qs, id) {
 }
 
 function getAllLabels(qs) {
-    return fetch(`https://graph.facebook.com/v2.11/me/custom_labels?fields=name&access_token=${qs}`, {
+    return fetch(`https://graph.facebook.com/v2.11/me/custom_labels?fields=name&${qs}`, {
         method: 'GET'
     })
     .then(rsp => rsp.json())
@@ -75,7 +75,7 @@ function createMessage(qs, message) {
             message
         ]
     });
-    return fetch(`https://graph.facebook.com/v2.11/me/message_creatives?access_token=${qs}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/me/message_creatives?${qs}`, options)
     .then(rsp => rsp.json())
     .then(json => validateJson(json));
 }
@@ -83,7 +83,7 @@ function createMessage(qs, message) {
 function sendMessage(qs, message, label) {
     const options = Object.assign({}, baseOptions);
     if(label) options.custom_label_id = label;
-    return fetch(`https://graph.facebook.com/v2.11/me/broadcast_messages?access_token=${qs}`, options)
+    return fetch(`https://graph.facebook.com/v2.11/me/broadcast_messages?${qs}`, options)
     .then(rsp => rsp.json())
     .then(json => validateJson(json)); 
 }
